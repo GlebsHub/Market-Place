4154ef297cfc903caf87079d17948bc9815ccba1
GlebsHub <gorbunovg@inbox.ru>
2022-06-16 10:50:05 +0300

    added tutorial

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 $(function(){
 
+$("#rateYo").rateYo({
+    rating: 5,
+    starwidth: '40px',
+    readOnly: true,
+});
 
 
+$('.product-slider__inner').slick({
+dots: true,
+arrows: false,
+sliderToShow: 4,
+sliderToScroll: 4,  
+});
+
+
+    
+var mixed = mixitup('.products__inner-box');
     
 });
